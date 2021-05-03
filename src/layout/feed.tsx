a2ba07f9b5df14eede485e4790956ad115db4e4a
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-05-03 17:56:58 -0300

    nicer looking posts

--- src/layout/feed.tsx
+++ src/layout/feed.tsx
@@ -23,6 +23,10 @@ const useStyles = makeStyles((theme: Theme) =>
         inline: {
             display: 'inline',
         },
+        flex: {
+            display: 'flex',
+            justifyContent: 'space-between',
+        }
     }),
 );
 
@@ -32,12 +36,24 @@ export default function Feed(props: Props) {
     return (
         <List className={classes.root}>
             {props.posts.map((row, index) => (
+                <div>
                 <ListItem alignItems="flex-start">
                     <ListItemAvatar>
                         <Avatar alt={row.name} src="/static/images/avatar/1.jpg" />
                     </ListItemAvatar>
                     <ListItemText
-                        primary={row.username}
+                        primary={
+                            <div className={classes.flex}>
+                                {row.username}
+                                <Typography
+                                    component="span"
+                                    variant="body2"
+                                    color="textPrimary"
+                                >
+                                    {row.date}
+                                </Typography>
+                            </div>
+                        }
                         secondary={
                             <React.Fragment>
                                 <Typography
@@ -46,59 +62,15 @@ export default function Feed(props: Props) {
                                     className={classes.inline}
                                     color="textPrimary"
                                 >
-                                    {row.date}
+                                    {row.text}
                                 </Typography>
-                                {row.text}
                             </React.Fragment>
                         }
                     />
                 </ListItem>
+                <Divider variant="inset" component="li" />
+                </div>
             ))}
-
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Summer BBQ"
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                component="span"
-                                variant="body2"
-                                className={classes.inline}
-                                color="textPrimary"
-                            >
-                                to Scott, Alex, Jennifer
-                            </Typography>
-                            {" — Wish I could come, but I'm out of town this…"}
-                        </React.Fragment>
-                    }
-                />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Oui Oui"
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                component="span"
-                                variant="body2"
-                                className={classes.inline}
-                                color="textPrimary"
-                            >
-                                Sandra Adams
-                            </Typography>
-                            {' — Do you have Paris recommendations? Have you ever…'}
-                        </React.Fragment>
-                    }
-                />
-            </ListItem>
         </List>
     );
 }
